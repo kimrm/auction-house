6424b3c418eba4493a3f2f125b85e2755adc69b3
Kim Rune Møller <kimrune@hotmail.no>
2023-11-15 13:04:47 +0100

    remove example unit tests

--- src/js/functions/coolFunc.js
+++
@@ -1,5 +0,0 @@
-const coolFunc = (a, b) => {
-  return a + b;
-};
-
-export default coolFunc;
--- src/js/functions/coolFunc.test.js
+++
@@ -1,8 +0,0 @@
-import coolFunc from "./coolFunc";
-
-describe("coolFunc", () => {
-  it("should add two numbers", () => {
-    const result = coolFunc(1, 2);
-    expect(result).toEqual(3);
-  });
-});
