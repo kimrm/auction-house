34c7aaf6d80cb923113eb26c9d49812de8a47557
Kim Rune Møller <kimrune@hotmail.no>
2023-12-11 02:03:16 +0100

    remove console.log

--- src/js/functions/api/register.js
+++ src/js/functions/api/register.js
@@ -2,7 +2,6 @@ import { authPath } from "./paths";
 import { post } from "./request";
 
 async function register(data) {
-  console.log(data);
   return await post(`${authPath}/register`, data);
 }
 
